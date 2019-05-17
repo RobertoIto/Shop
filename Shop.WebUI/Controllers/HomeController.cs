38079ad6a16993012181a93256285e982628598f
Rito <roberto_ito@hotmail.com>
2019-05-17 09:51:44 -0400

    Created filter for the products by category

--- Shop.WebUI/Controllers/HomeController.cs
+++ Shop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Shop.Core.Contracts;
 using Shop.Core.Models;
+using Shop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,10 +24,24 @@ namespace Shop.WebUI.Controllers
             productCategories = productCategoryContext;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if (Category == null)
+            {
+                products = context.Collection().ToList();
+            } else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model);
         }
 
         public ActionResult Details(string Id)
